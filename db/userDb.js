a02144b7fd7fcef79c9121121561dd9d406726c6
Khushwant <khushu1501@gmail.com>
2023-07-07 08:18:29 -0400

    User authentication database

---
+++ db/userDb.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+let mongoDBConnectionString = process.env.MONGODB_CONN_STRING;
+
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  userName: {
+    type: String,
+    unique: true,
+  },
+  password: String,
+  history: [String],
+});
+
+let User;
+
+module.exports.connect = function () {
+  return new Promise(function (resolve, reject) {
+    let db = mongoose.createConnection(mongoDBConnectionString);
+
+    db.on("error", (err) => {
+      reject(err);
+    });
+
+    db.once("open", () => {
+      User = db.model("users", userSchema);
+      resolve();
+    });
+  });
+};
+
+module.exports.checkUser = function (userData) {
+  return new Promise(function (resolve, reject) {
+    User.findOne({ userName: userData.userName })
+      .exec()
+      .then((user) => {
+        bcrypt.compare(userData.password, user.password).then((res) => {
+          if (res === true) {
+            resolve(user);
+          } else {
+            reject("Incorrect password for user " + userData.userName);
+          }
+        });
+      })
+      .catch((err) => {
+        reject("Unable to find user " + userData.userName);
+      });
+  });
+};
